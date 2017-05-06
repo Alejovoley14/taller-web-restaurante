0d7ce4470b9106fb32cd7b9fa5f8be1f277cfde6
Nicolás Cardozo <ncardozo19@gmail.com>
2017-05-06 18:36:54 -0300

    arreglo de problemas

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Usuario.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Usuario.java
@@ -1,42 +1,15 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
-<<<<<<< HEAD
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-@Entity
-public class Usuario {
-	
-	@Id 
-	@GeneratedValue(strategy = GenerationType.IDENTITY) 
-	private Long id;
-	private String email;
-	private String password;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-=======
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Collection;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 @Entity(name = "Usuario")
 public class Usuario extends Entidad {
@@ -146,5 +119,5 @@ public class Usuario extends Entidad {
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
     }
->>>>>>> origin/master
+
 }
