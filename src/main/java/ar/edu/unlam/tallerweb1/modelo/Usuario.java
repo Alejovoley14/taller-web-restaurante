1585a91f172b277b8cd21b89b02a4a5e015cfc73
Nicolás Cardozo <ncardozo19@gmail.com>
2017-05-06 15:31:14 -0300

    arreglo de problemas con el usuario model

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Usuario.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Usuario.java
@@ -1,39 +1,9 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
-<<<<<<< HEAD
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-@Entity //indica que esto se procesara en una base de datos
-public class Usuario {
-	
-	@Id //indica la primary key de la tabla en que se guardara
-	@GeneratedValue(strategy = GenerationType.IDENTITY) //esto ganera el id en los registro de la bd de forma autoincremental
-	private Long id;
-	private String email;
-	private String password;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-=======
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -140,11 +110,10 @@ public class Usuario extends Entidad {
     }
 
     public Cliente getCliente() {
-        return cliente;
-    }
+     	return cliente;
+   }
 
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
     }
->>>>>>> origin/master
 }
